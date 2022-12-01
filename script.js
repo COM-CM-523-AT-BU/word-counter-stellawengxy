b1923fcbc8885959d7e2c038e9ac8cda555068e8
stellawengxy <xweng@bu.edu>
2022-11-30 22:04:58 -0500

    change

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 // put your globals here - hint: select elements from the HTML
-
+const wordInput = document.querySelector("#text-area");
+const wordCount = document.querySelector("#wordCountInput");
+const countBtn = document.querySelector("#countBtn");
+const findWord = document.querySelector("#findWordInput");
+const findBtn = document.querySelector("#findBtn");
+const wordFound = document.querySelector("#wordFoundInput");
 
 // dont' forget to add event listeners to teh buttons
+countBtn.addEventListener("click", countWords);
+findBtn.addEventListener("click", findWords);
+
+
 
 function countWords() {
-  // your code here
-  // call printData
+ 
+  let str = wordInput.value;
+  const words = str.split(' ');
+  const count = words.length;
+  printData(count, wordCount);
 }
+
 
 function findWords() {
-  // put your local variable for the empty array here
-  // remainder of your code follows
-  // call printData
+  let selectedwords = [];
+  let str = wordInput.value;
+  const words = str.split(' ');
+  words.forEach( (item) => {
+    if(item === findWord.value){
+      selectedwords.push(item);
+    }
+  });
+  printData(selectedwords.length, wordFound);
 }
+ 
 
 // change param1 and param2 to identifiers that make sense
-function printData(param1, param2) {
-  // your code here... one line!
+function printData(data, input) { 
+  input.value = data;
 }
+
+
+
+
+ 
